04a7b09d5bb00ad2c7e01b0134d51780c3abdc5f
franeum <elettrochuck@gmail.com>
2021-03-06 16:44:55 +0100

    add os differences

--- 2021_puredata/test.py
+++ 2021_puredata/test.py
@@ -5,9 +5,15 @@ import os
 import time
 import random
 import re
+import sys
 
-#PDPATH = "/Applications/Pd-l2ork.app/Contents/MacOS/nwjs main.pd &"
-PDPATH = "pd main.pd &"
+if sys.platform == 'darwin':
+    PDPATH = "/Applications/Pd-l2ork.app/Contents/MacOS/nwjs main.pd &"
+elif sys.platform == 'linux':
+    PDPATH = "pd main.pd &"
+elif sys.platform == 'win32':
+    exit()
+
 PATTERN1 = re.compile(r'file (.+);')
 UDP_IP = "127.0.0.1"
 UDP_SEND_PORT = 8000
@@ -15,15 +21,6 @@ UDP_RCV_PORT = 8001
 
 if __name__ == "__main__":
     os.system(PDPATH)
-    #sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
-    """for _ in range(10):
-        token = str.encode(str(random.randint(0, 100)))
-        sock.sendto(token, (UDP_IP, UDP_PORT))
-        print("send", token)
-        time.sleep(1)"""
-
-    # sock.close()
 
     sock_srv = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # UDP
     sock_srv.bind((UDP_IP, UDP_RCV_PORT))
@@ -37,7 +34,7 @@ if __name__ == "__main__":
             res = re.match(PATTERN1, data)
             if res:
                 print(f"received message: {res.group(1)}")
-                sock_srv.sendto(b'received', ("127.0.0.1",8000))
+                sock_srv.sendto(b'received', (UDP_IP, 8000))
         except KeyboardInterrupt:
             sock_srv.close()
             exit()
